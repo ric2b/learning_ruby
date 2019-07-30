1833c8bde5e3cc7b4d997ed5dc6874ee8988927a
Ricardo Amendoeira <ricardo.amendoeira@ist.utl.pt>
2019-07-31 00:12:03 +0100

    Add docker-compose with rabbitmq, send and receive messages to it

--- echoer/src/main.rb
+++ echoer/src/main.rb
@@ -1,7 +1,42 @@
-def hello
-  'hello'
+#!/usr/bin/env ruby
+require 'bunny'
+
+def send(message)
+  connection = Bunny.new(hostname: ENV['BROKER_HOSTNAME'] || 'localhost')
+
+  attempts = 0
+  begin
+    connection.start
+  rescue Bunny::TCPConnectionFailed
+    $stdout.flush
+    sleep(2**attempts)
+    retry if (attempts += 1) < 6
+    raise IOError("Unable to connect")
+  end
+
+  channel = connection.create_channel
+  queue = channel.queue('hello')
+  channel.default_exchange.publish('Hello World!', routing_key: queue.name)
+  puts " [x] Sent 'Hello World!'"
+
+  connection.close
+end
+
+def receive
+  connection = Bunny.new(hostname: ENV['BROKER_HOSTNAME'] || 'localhost')
+  connection.start
+
+  channel = connection.create_channel
+  queue = channel.queue('hello')
+  puts ' [*] Waiting for message.'
+  queue.subscribe(block: true) do |_delivery_info, _properties, body|
+    puts " [x] Received message: #{body}"
+    connection.close
+  end
 end
 
 if __FILE__ == $0
-  puts hello
+  puts 'starting'
+  send 'hello bunny'
+  puts "received: #{receive}"
 end
--- echoer/test/test_main.rb
+++ echoer/test/test_main.rb
@@ -4,6 +4,7 @@ require_relative '../src/main'
 
 class MainTest < Minitest::Test
   def test_main_says_hello
+    skip # no longer what main does
     assert_equal 'hello', hello
   end
 end
